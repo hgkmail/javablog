6d1d21894b36cbafdc9b1bf2d510d632250b1591
coderkim <hgkformap@gmail.com>
2019-08-31 01:14:55 +0800

    init

---
+++ src/main/java/coder/kim/javablog/JavablogApplication.java
@@ -0,0 +1,13 @@
+package coder.kim.javablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavablogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavablogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/coder/kim/javablog/JavablogApplicationTests.java
@@ -0,0 +1,16 @@
+package coder.kim.javablog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavablogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
